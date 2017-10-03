647c752588c95928b225268a9f3f41731ee33931
George Blackwell <georgeblackwell194@gmail.com>
2017-10-03 10:48:25 +0100

    main game files - DON'T FORGET TO BRANCH MATT

---
+++ Arcade White Box/Assets/Scripts/Tile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public enum TileType { Empty, Passable, Impassable, Object, Wall }
+
+    [SerializeField] private TileType tileType;
+
+    private Vector2 tileCoordinates;
+
+    public TileType GetTileType()
+    {
+        return tileType;
+    }
+
+    public Vector2 GetCoordinates()
+    {
+        return tileCoordinates;
+    }
+
+    public void SetTileType(TileType newType)
+    {
+        tileType = newType;
+    }
+
+    public void SetCoordinates(Vector2 tileCoordinates)
+    {
+        this.tileCoordinates = tileCoordinates;
+    }
+}
