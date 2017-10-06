3ad83a2b613e681776417cfc00dc2c055bce018a
Matt Watling <mat157@hotmail.co.uk>
2017-10-06 10:05:10 +0100

    UI ReAdded

---
+++ Arcade White Box/Assets/Scripts/Objects/PlaceableObject.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceableObject : Entity {
+
+    [SerializeField] protected float buyCost; // how much the object costs to buy.
+    [SerializeField] protected GameObject selectionMesh; // the mesh that will flash when the player selects this object in the world.
+    [SerializeField] protected int percentReturnedUponSold; //PERCENTAGE amount returned upon being sold back to the manufacturer. 
+
+    protected bool selected;
+    protected Tile placedOnTile;
+    protected Transform selectionTransform;
+    protected PlayerManager _playerLink;
+
+    protected virtual void Awake()
+    {
+        selectionTransform = selectionMesh.GetComponent<Transform>();
+        selectionMesh.SetActive(false);
+        Selected = false;
+        _playerLink = GameObject.Find("Game Manager").GetComponent<PlayerManager>();
+    }
+
+    protected virtual void Update()
+    {
+        if (Selected)
+        {
+            PulseScale();
+        }
+    }
+
+    protected void OnMouseOver()
+    {
+        selectionMesh.SetActive(true);
+    }
+
+    protected void OnMouseExit()
+    {
+        if (!Selected)
+        {
+            selectionMesh.SetActive(false);
+        }
+    }
+
+    protected void PulseScale()
+    {
+        selectionTransform.localScale = new Vector3(Mathf.PingPong(Time.time, 0.2f) + 1.0f, Mathf.PingPong(Time.time, 0.2f) + 1.0f, Mathf.PingPong(Time.time, 0.2f) + 1.0f);
+    }
+
+    public bool Selected
+    {
+        get
+        {
+            return selected;
+        }
+
+        set
+        {
+            selected = value;
+
+            if (!selected)
+            {
+                selectionTransform.localScale = new Vector3(1, 1, 1);
+                selectionMesh.SetActive(false);
+            }
+        }
+    }
+
+    public Tile PlacedOnTile
+    {
+        get
+        {
+            return placedOnTile;
+        }
+
+        set
+        {
+            placedOnTile = value;
+        }
+    }
+
+    public float BuyCost
+    {
+        get
+        {
+            return buyCost;
+        }
+
+        set
+        {
+            buyCost = value;
+        }
+    }
+
+    public float returnAmount()
+    {
+        float onePercent = BuyCost / 100;
+        return onePercent * percentReturnedUponSold;
+
+    }
+}
---
+++ Arcade White Box/Assets/Scripts/Stats & Game Managment/PlayerManager.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour {
+
+    [SerializeField] private string playerName, arcadeName;
+    [SerializeField] private float startingCash;
+
+    [SerializeField] private Text cashText, earningsText, expensesText;
+
+    private float currentCash, currentlyEarnedToday, currentExpenses;
+
+    public float CurrentCash
+    {
+        get
+        {
+            return currentCash;
+        }
+
+        set
+        {
+            currentCash = value;
+        }
+    }
+
+    public float CurrentlyEarnedToday
+    {
+        get
+        {
+            return currentlyEarnedToday;
+        }
+
+        set
+        {
+            currentlyEarnedToday = value;
+        }
+    }
+
+    public float CurrentExpenses
+    {
+        get
+        {
+            return currentExpenses;
+        }
+
+        set
+        {
+            currentExpenses = value;
+        }
+    }
+
+    public string PlayerName
+    {
+        get
+        {
+            return playerName;
+        }
+
+        set
+        {
+            playerName = value;
+        }
+    }
+
+    public string ArcadeName
+    {
+        get
+        {
+            return arcadeName;
+        }
+
+        set
+        {
+            arcadeName = value;
+        }
+    }
+
+    void Start () {
+
+        CurrentCash = startingCash;
+ 
+		
+	}
+	
+	void Update () {
+        UpdateCashText();
+        UpdateEarningsText();
+        UpdateExpensesText();
+		
+	}
+
+    private void UpdateCashText()
+    {
+        cashText.text = CurrentCash.ToString();
+    }
+
+    private void UpdateEarningsText()
+    {
+        earningsText.text = CurrentlyEarnedToday.ToString();
+    }
+
+    private void UpdateExpensesText()
+    {
+        expensesText.text = CurrentExpenses.ToString();
+    }
+
+    public bool CheckCanAfford(float price)
+    {
+        if (CurrentCash - price > -1)
+            return true;
+
+        else
+            return false;
+    }
+
+    // when a machine has been purchased;
+    public void OnMachinePurchase(Machine purchase)
+    {
+        CurrentCash -= purchase.BuyCost;
+        currentExpenses += purchase.RunningCost;
+    }
+
+    public void OnBuildingPartPurchase(PlaceableObject purchase)
+    {
+        CurrentCash -= purchase.BuyCost;
+    }
+
+    public void ClosingTime()
+    {
+        CurrentCash -= CurrentExpenses;
+        CurrentlyEarnedToday = 0;
+    }
+ 
+    
+}
