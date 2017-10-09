94d7685c1c43adec7734e4f6d2545089431e70d7
Nathaniel Whittaker <natwuniversity@gmail.com>
2017-10-09 16:57:54 +0100

    Background and First Animation

---
+++ Arcade White Box/Assets/Scripts/UI/Notification Stuff/NotificationAssignment.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationAssignment : MonoBehaviour {
+
+    private string[] posArray, negArray, specArray;
+    private List<Image> allIconImages;
+
+    [SerializeField]
+    private GameObject notificationBanner;
+
+    [SerializeField]
+    private Text displayTextField;
+
+   // [SerializeField]
+   // private Image displayImageField;
+
+    [SerializeField]
+    private float lengthOfNotification, timeBetweenNotifications;
+
+	// Use this for initialization
+	void Start () {
+        posArray = this.GetComponent<NotificationLoader>().PosArray;
+        negArray = this.GetComponent<NotificationLoader>().NegArray;
+        specArray = this.GetComponent<NotificationLoader>().SpecArray;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void randomImage()
+    {
+        // displayImageField = allIconImages[Random.Range(0, allIconImages.Count)];
+    }
+
+    public void positiveNotificationTrigger()
+    {
+        displayTextField.text = posArray[Random.Range(0, posArray.Length)];
+        //randomImage();
+    }
+
+    public void negativeNotificationTrigger()
+    {
+        displayTextField.text = negArray[Random.Range(0, negArray.Length)];
+       // randomImage();
+    }
+
+    public void specialNotificationTrigger()
+    {
+        // might to specific messages here. Will have to figure out a way of numbering them. Possibly just use the array ordering?
+        displayTextField.text = specArray[Random.Range(0, specArray.Length)];
+        // maybe a special image here? warning image/event based?
+    }
+}
---
+++ Arcade White Box/Assets/Scripts/UI/Notification Stuff/NotificationLoader.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.IO;
+using System;
+
+public class NotificationLoader : MonoBehaviour {
+
+    private string[] posArray, negArray, specArray;
+
+    [SerializeField]
+    private TextAsset positiveNotifications, negativeNotifications, specialNotifications;
+
+    public string[] PosArray
+    {
+        get
+        {
+            return posArray;
+        }
+
+        set
+        {
+            posArray = value;
+        }
+    }
+
+    public string[] NegArray
+    {
+        get
+        {
+            return negArray;
+        }
+
+        set
+        {
+            negArray = value;
+        }
+    }
+
+    public string[] SpecArray
+    {
+        get
+        {
+            return specArray;
+        }
+
+        set
+        {
+            specArray = value;
+        }
+    }
+
+    void Start()
+    {
+        string posText = positiveNotifications.text;
+        splitStrings(posText, PosArray);
+
+        string negText = negativeNotifications.text;
+        splitStrings(negText, NegArray);
+
+        string specText = specialNotifications.text;
+        splitStrings(specText, SpecArray);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void splitStrings(string stringToSplit, string[] array)
+    {
+        array = stringToSplit.Split('\n');
+        //printAllNotifications(array);
+    }
+
+   
+    private void printAllNotifications(string[] array)
+    {
+        foreach (string str in array)
+        {
+            print(str);
+        }
+    }
+
+    
+}
+
