bc83fd8c42cc6b6745ddf39f67b81b08e8312262
George Blackwell <georgeblackwell194@gmail.com>
2017-10-17 14:46:34 +0100

    customer loop almost finished

--- Arcade White Box/Assets/Scripts/AI/Customer.cs
+++ Arcade White Box/Assets/Scripts/AI/Customer.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Customer : BaseAI
 {   
-    [SerializeField] private float needsTickRate;
-    [SerializeField] private Transform[] testFacilities;
+    [SerializeField] private float needsTickAmount, needsTickRate, needLimit;
 
-    private bool usingFacility;
+    private enum CustomerState { Idle, Moving, UsingFacility, TimesUp }
+
     private float needsCounter;
-    private float foodSensitivity, toiletSensitivity, excitmentSensitivity;
+    private CustomerState currentState;
     private CustomerNeed[] customerNeeds;
+    private CustomerNeed highestNeed;
     private Unit unitController;
     private CustomerText customerText;
-    private Transform customerTransform;
+    private Transform customerTransform, doorPosition;
+    private Transform spawnLocation;
     private IEnumerator usingFacilityWait; 
 
     void Start()
@@ -22,72 +24,65 @@ public class Customer : BaseAI
         customerText = GetComponent<CustomerText>();
         customerTransform = GetComponent<Transform>();
 
-        // -- CREATING & ADDING NEEDS TO ARRAY --
         customerNeeds = new CustomerNeed[3];
 
-        customerNeeds[0] = new CustomerNeed(CustomerNeed.NeedType.Food, Random.Range(0.0f, 30.0f));
-        customerNeeds[1] = new CustomerNeed(CustomerNeed.NeedType.Toilet, Random.Range(0.0f, 30.0f));
-        customerNeeds[2] = new CustomerNeed(CustomerNeed.NeedType.Excitement, Random.Range(0.0f, 30.0f));
-        // --------------------------------------
+        customerNeeds[0] = new CustomerNeed(CustomerNeed.NeedType.Food, Random.Range((needsTickRate * 2.0f) + 1.0f, needLimit - (needsTickRate * 2.0f)));
+        customerNeeds[1] = new CustomerNeed(CustomerNeed.NeedType.Toilet, Random.Range((needsTickRate * 2.0f) + 1.0f, needLimit - (needsTickRate * 2.0f)));
+        customerNeeds[2] = new CustomerNeed(CustomerNeed.NeedType.Excitement, Random.Range((needsTickRate * 2.0f) + 1.0f, needLimit - (needsTickRate * 2.0f)));
 
-        // ----- SETTING SENSITIVITY VALUES -----
-        foodSensitivity = Random.Range(1.0f, 10.0f);
-        toiletSensitivity = Random.Range(1.0f, 10.0f);
-        excitmentSensitivity = Random.Range(1.0f, 10.0f);
-        // --------------------------------------
+        customerNeeds[Random.Range(0, customerNeeds.Length)].NeedValue = needLimit;
 
-        needsCounter = needsTickRate;
+        needsCounter = needsTickAmount;
+        currentState = CustomerState.Idle;
     }
 
     void Update()
     {
-        if(!usingFacility)
+        if(currentState == CustomerState.Idle)
         {
-            NeedsTick();
+            CustomerNeed customerNeed = GetHightestNeed();
 
-            if(GetHightestNeed().NeedValue >= 120.0f)
+            if (customerNeed.NeedValue >= needLimit)
             {
-                GetNextState();
+                highestNeed = customerNeed;
+
+                if ((int)customerNeed.Need == (int)CustomerNeed.NeedType.Food)
+                {
+                    unitController.SetTarget(FindNearestFacility(CustomerManager.instance.GetFoodStalls()));
+                }
+                else if ((int)customerNeed.Need == (int)CustomerNeed.NeedType.Toilet)
+                {
+                    unitController.SetTarget(FindNearestFacility(CustomerManager.instance.GetToilets()));
+                }
+                else if ((int)customerNeed.Need == (int)CustomerNeed.NeedType.Excitement)
+                {
+                    unitController.SetTarget(FindNearestFacility(CustomerManager.instance.GetGameMachines()));
+                }
+
+                unitController.GetNewPath();
+                currentState = CustomerState.Moving;
+            }
+        }
+        else if(currentState == CustomerState.Moving)
+        {   
+            if(unitController.ReachedTarget)
+            {   
+                usingFacilityWait = UsingFacilitiesWait(10.0f);
+                StartCoroutine(usingFacilityWait);
             }
         }
 
-        if(Input.GetKeyDown(KeyCode.K))
-        {
-            unitController.SetTarget(FindNearestFacility(testFacilities));
-            unitController.GetNewPath();
-        }
+         NeedsTick();
     }
 
-    private void GetNextState()
+    public void LeaveArcade()
     {
-        CustomerNeed need = GetHightestNeed();
+        currentState = CustomerState.TimesUp;
+    }
 
-        if((int)need.Need == (int)CustomerNeed.NeedType.Food)
-        {
-            print("Customer - " + Name1 + "s' current highest need is FOOD - " + need.NeedValue);
-            customerText.ShowFoodText();
-            unitController.SetTarget(FindNearestFacility(testFacilities));
-            unitController.GetNewPath();
-
-        }
-        else if((int)need.Need == (int)CustomerNeed.NeedType.Toilet)
-        {
-            print("Customer - " + Name1 + "s' current highest need is TOILET - " + need.NeedValue);
-            customerText.ShowToiletText();
-            unitController.SetTarget(FindNearestFacility(testFacilities));
-            unitController.GetNewPath();
-        }
-        else if((int)need.Need == (int)CustomerNeed.NeedType.Excitement)
-        {
-            print("Customer - " + Name1 + "s' current highest need is EXCITEMENT - " + need.NeedValue);
-            customerText.ShowExcitementText();
-            unitController.SetTarget(FindNearestFacility(testFacilities));
-            unitController.GetNewPath();
-        }
-        else
-        {
-            print("NO NEED COULD BE FOUND FOR: " + Name1);
-        }
+    public void SetSpawnLocation(Transform spawnLocation)
+    {
+        this.spawnLocation = spawnLocation;
     }
 
     private CustomerNeed GetHightestNeed()
@@ -111,16 +106,12 @@ public class Customer : BaseAI
         
         if(needsCounter <= 0.0f)
         {
-            customerNeeds[0].NeedValue += foodSensitivity * 1.1f;
-            print("FOOD LVLS - " + customerNeeds[0].NeedValue);
+            foreach (CustomerNeed need in customerNeeds)
+            {
+                need.NeedValue += needsTickRate;
+            }
 
-            customerNeeds[1].NeedValue += toiletSensitivity * 1.1f;
-            print("TOILET LVLS - " + customerNeeds[1].NeedValue);
-
-            customerNeeds[2].NeedValue += excitmentSensitivity * 1.1f;
-            print("EXCITEMENT LVLS - " + customerNeeds[2].NeedValue);
-
-            needsCounter = needsTickRate;
+            needsCounter = needsTickAmount;
         }
     }
 
@@ -136,14 +127,14 @@ public class Customer : BaseAI
             }
         }
 
-        print(nearest.name);
         return nearest;
     }
 
     private IEnumerator UsingFacilitiesWait(float waitTime)
     {
-        usingFacility = true;
+        highestNeed.NeedValue = 0.0f;
+        currentState = CustomerState.UsingFacility;
         yield return new WaitForSeconds(waitTime);
-        usingFacility = false;
+        currentState = CustomerState.Idle;
     }
 }
--- Arcade White Box/Assets/Scripts/AI/CustomerManager.cs
+++ Arcade White Box/Assets/Scripts/AI/CustomerManager.cs
@@ -4,5 +4,66 @@ using UnityEngine;
 
 public class CustomerManager : MonoBehaviour
 {
-    private List<Customer> customers;
+    public static CustomerManager instance;
+       
+    [SerializeField] private Customer customer;
+    [SerializeField] private Transform spawnLocation;
+    [SerializeField] private Transform[] toilets, foodStalls, gameMachines;
+
+    private List<Customer> currentCustomers;
+
+    void Awake()
+    {
+        instance = this;
+
+        currentCustomers = new List<Customer>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            MassLeave();
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            SpawnCustomers(1);
+        }
+    }
+
+    public void SpawnCustomers(int amount)
+    {
+        for(int i = 0; i < amount; i++)
+        {
+            Customer newCustomer = Instantiate(customer, spawnLocation.position, Quaternion.identity) as Customer;
+            newCustomer.SetSpawnLocation(spawnLocation);
+            currentCustomers.Add(newCustomer);
+        }
+    }
+
+    public void MassLeave()
+    {
+        foreach(Customer currentCustomer in currentCustomers)
+        {   
+            currentCustomer.LeaveArcade();
+        }
+
+        currentCustomers.Clear();
+    }
+
+    public Transform[] GetToilets()
+    {
+        return toilets;
+    }
+
+    public Transform[] GetFoodStalls()
+    {
+        return foodStalls;
+    }
+
+    public Transform[] GetGameMachines()
+    {
+        return gameMachines;
+    }
 }
