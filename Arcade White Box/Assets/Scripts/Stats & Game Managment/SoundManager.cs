0e84d816c01098b0632d72b93105eb8b43a8fb2a
Matt Watling <mat157@hotmail.co.uk>
2017-10-18 15:30:20 +0100

    new shit dude

--- Arcade White Box/Assets/Scripts/Stats & Game Managment/SoundManager.cs
+++ Arcade White Box/Assets/Scripts/Stats & Game Managment/SoundManager.cs
@@ -5,25 +5,94 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour {
 
     
-    public AudioClip[] menuAudioClip;    
+    public AudioClip[] menuAudio;    
     public AudioClip[] inGameAudio;
 
+    //private AudioSource menuAudioSource;
     private AudioSource gameAudioSource;
+
+    private int trackNumber = 0;
+
+    public string currentSongName;
+
+    public float fadeDuration;
 
     void Start()
     {
     }
+
+    void Update()
+    {
+        //InGameAudio();
+    }
     
     public void MenuAudio()
     {
-        gameAudioSource.clip = menuAudioClip[Random.Range(0, menuAudioClip.Length)];
-        gameAudioSource.PlayDelayed(0.0f);
+        gameAudioSource.clip = menuAudio[Random.Range(0, menuAudio.Length)];
+        gameAudioSource.Play();
     }
 
     public void InGameAudio()
     {
-        gameAudioSource.clip = inGameAudio[Random.Range(0, inGameAudio.Length)];
-        gameAudioSource.PlayDelayed(0.0f);
+        if(!gameAudioSource.isPlaying && trackNumber < inGameAudio.Length)
+        {
+            gameAudioSource.clip = inGameAudio[trackNumber];
+            gameAudioSource.Play();
+            StartCoroutine(FadeIn());
+            currentSongName = gameAudioSource.clip.ToString();
+            print("The current song is " + currentSongName);
+            trackNumber++;
+        }
+        else
+        {
+            //StartCoroutine(FadeOut());
+        }
+        if (trackNumber == inGameAudio.Length)
+        {
+            trackNumber = 0;
+        }
+        
+        //gameAudioSource.PlayDelayed(0.0f);
+    }   
+
+    private IEnumerator FadeOut()
+    {
+        if (fadeDuration > 0f)
+        {
+            float startTime = gameAudioSource.clip.length - fadeDuration;
+            float lerpValue = 0f;
+            while (lerpValue < 1f)
+            {
+                lerpValue = Mathf.InverseLerp(startTime, gameAudioSource.clip.length, gameAudioSource.time);
+                gameAudioSource.volume = Mathf.Lerp(gameAudioSource.volume, 0f, lerpValue);
+                yield return null;
+            }
+        }
+    }
+
+    private IEnumerator FadeIn()
+    {
+        if (fadeDuration == 0f)
+        {
+            print("fade in 1");
+            float lerpValue = 0f;
+            while (lerpValue < 1f)
+            {
+                lerpValue = Mathf.InverseLerp(0f, fadeDuration, gameAudioSource.time);
+                gameAudioSource.volume = Mathf.Lerp(0f, gameAudioSource.volume, lerpValue);
+                print("fade in 2");
+                yield return null;
+            }
+        }
+        else
+        {
+            yield break;
+        }
+    }
+
+    public void NextSong()
+    {
+        gameAudioSource.clip = inGameAudio[0];
     }
 
     public void setAudioSource(AudioSource newAudioSource)
